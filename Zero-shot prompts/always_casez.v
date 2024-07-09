47ce69f120028d895001d857d43588bd87484214
Fatima Farooq <ff2137@nyu.edu>
2024-07-09 15:39:14 -0400

    completed zero-shot prompts

---
+++ Zero-shot prompts/always_casez.v
@@ -0,0 +1,15 @@
+// you're a senior hardware engineer at a leading hardware design company tasked with developing a critical Verilog module for a next-generation product. The success of this module is pivotal for maintaining the company's reputation in the industry.
+
+// Build a priority encoder for 8-bit inputs. Given an 8-bit vector, 
+// the output should report the first (least significant) bit in the vector that is 1. 
+// Report zero if the input vector has no bits that are high. 
+// For example, the input 8'b10010000 should output 3'd4, because bit[4] is first bit that is high.
+
+// synthesis verilog_input_version verilog_2001
+module top_module (
+    input [7:0] in,
+    output reg [2:0] pos );
+
+    // Insert your code here
+
+endmodule
---
+++ Zero-shot prompts/mux256to1.v
@@ -0,0 +1,20 @@
+// you're a senior hardware engineer at a leading hardware design company tasked with developing a critical Verilog module for a next-generation product. The success of this module is pivotal for maintaining the company's reputation in the industry.
+
+// Create a 1-bit wide, 256-to-1 multiplexer. The 256 inputs are all packed into a 
+// single 256-bit input vector. sel=0 should select in[0], sel=1 selects bits in[1], 
+// sel=2 selects bits in[2], etc.
+
+// Hint: With this many options, a case statement isn't so useful.
+// Vector indices can be variable, as long as the synthesizer can figure out that 
+// the width of the bits being selected is constant. In particular, 
+// selecting one bit out of a vector using a variable index will work.
+
+
+module top_module( 
+    input [255:0] in,
+    input [7:0] sel,
+    output out );
+
+    //Insert your code here
+
+endmodule
